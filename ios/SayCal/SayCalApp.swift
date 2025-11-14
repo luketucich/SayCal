c548d67bddaca4db876acb7cf921e3b5356e9df0
luketucich <luketucich@gmail.com>
2025-11-14 00:44:43 -0500

    chore: create swift project

---
+++ ios/SayCal/SayCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayCalApp.swift
+//  SayCal
+//
+//  Created by Luke on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SayCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
